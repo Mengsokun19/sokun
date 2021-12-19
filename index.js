6cb3ceb87f3d2ab83db34fa45502b0f037a57174
Mengsokun19 <61143622+Mengsokun19@users.noreply.github.com>
2021-12-19 15:18:32 +0700

    first version

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+console.log(`
+Veng Mengsokun
+
+I am a third year Software Engineering Student
+From Kirirom Institute of Technology(KIT), who 
+have experience with JavaScript and the Node JS 
+Framework. This is my first Node-CLI. I hope that
+I can learn more about this with enjoy creating 
+CLI to gain more experience with Node JS and have
+fun with it.
+
+
+`)
